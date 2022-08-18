cc134590cbbddaf5d31a4b8d8e2f0fbaf6969fed
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-08-18 00:48:25 -0300

    fixeando bug

--- src/socket.js
+++ src/socket.js
@@ -1,37 +1,4 @@
 module.exports = (io) => {
   let messages = [];
   let usersConnected = [];
-
-  io.on('connection', (socket) => {
-    io.emit('messages', messages);
-    console.log('connection', socket.id);
-
-    // socket.join("global chat")
-
-    socket.on('userLogged', (userData) => {
-      const user = {
-        username: userData.username,
-        userId: userData.uuid,
-      };
-      usersConnected.push(user);
-    });
-
-    //rooms
-
-    socket.on('userLogOut', (userData) => {
-      const filterUsers = usersConnected.filter(
-        (us) => us.uuid !== userData.uuid
-      );
-      usersConnected = filterUsers;
-    });
-
-    socket.on('message', (messageData) => {
-      messages.push(messageData);
-      io.emit('messages', messages);
-    });
-    socket.on('disconnect', () => {
-      usersConnected = usersConnected.filter((u) => u.id !== socket.id);
-      io.emit('new user', usersConnected);
-    });
-  });
 };
