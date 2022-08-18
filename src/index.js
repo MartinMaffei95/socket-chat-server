ad3569aa4b830f9b851122386c4c90c950b2492f
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-08-18 00:29:17 -0300

    probando slash

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: '*',
+    origin: '*', //TEMP : Change for my domain
     methods: ['GET', 'POST'],
   },
 });
 require('./socket')(io);
 
+app.get('/', function (req, res) {
+  res.send(200, 'ok');
+});
 server.listen(app.get('port'), () => {
   console.log('App run in port:' + app.get('port'));
 });
