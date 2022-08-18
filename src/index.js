75414c979beda1ff695ddcca45169b0fdf785342
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-08-18 00:46:43 -0300

    fixeando bug

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
 const socketio = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-
 const server = http.createServer(app);
 const io = socketio(server);
+// const io = socketio(server, {
+//   cors: {
+//     origin: '*', //TEMP : Change for my domain
+//     methods: ['GET', 'POST'],
+//   },
+// });
 require('./socket')(io);
 
-app.set('port', 3005);
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 server.listen(app.get('port'), () => {
-  console.log('App run in port:' + app.get('port'));
+  console.log('corriendo en el puerto: ' + app.get('port'));
 });
