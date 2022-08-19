dc3eb4fbf6dbf17cfe92c36218f37128d67e0e2f
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-08-19 19:22:02 -0300

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const http = require('http');
 
-app.set('port', 3005);
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
