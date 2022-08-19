91bafe8a805b6a500162fcd6ef78bc78f5771476
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2022-08-19 18:57:22 -0300

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 const express = require('express');
-const http = require('http');
-const path = require('path');
 const socketio = require('socket.io');
-const cors = require('cors');
-
+const path = require('path');
 const app = express();
+const http = require('http');
+
+app.set('port', 3005);
+app.use(express.static(path.join(__dirname, 'public')));
+
 const server = http.createServer(app);
-const io = socketio(server);
-// const io = socketio(server, {
-//   cors: {
-//     origin: '*', //TEMP : Change for my domain
-//     methods: ['GET', 'POST'],
-//   },
-// });
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 require('./socket')(io);
 
-app.set('port', process.env.PORT || 5000);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
 server.listen(app.get('port'), () => {
-  console.log('corriendo en el puerto: ' + app.get('port'));
+  console.log('App run in port:' + app.get('port'));
 });
--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,45 @@
 module.exports = (io) => {
   let messages = [];
   let usersConnected = [];
+
+  io.on('connection', (socket) => {
+    io.emit('messages', messages);
+    console.log('connection', socket.id);
+
+    // socket.join("global chat")
+
+    socket.on('userLogged', (userData) => {
+      const user = {
+        username: userData.username,
+        userId: userData.uuid,
+      };
+      usersConnected.push(user);
+      io.emit('new user', usersConnected);
+    });
+
+    //rooms
+
+    socket.on('join room', (roomName, callback) => {
+      socket.join(roomName);
+      callback(messages[roomName]);
+      socket.emit('joined', messages[roomName]);
+    });
+
+    socket.on('userLogOut', (userData) => {
+      const filterUsers = usersConnected.filter(
+        (us) => us.uuid !== userData.uuid
+      );
+      usersConnected = filterUsers;
+    });
+
+    socket.on('message', (messageData) => {
+      messages.push(messageData);
+      io.emit('messages', messages);
+      console.log(messages);
+    });
+    socket.on('disconnect', () => {
+      usersConnected = usersConnected.filter((u) => u.id !== socket.id);
+      io.emit('new user', usersConnected);
+    });
+  });
 };
